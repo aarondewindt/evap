1a5730adef3cfe37de86e7687b45c6db062664ee
Aaron de Windt <aaron@delft-circuits.com>
2024-07-21 20:47:15 +0000

    fixed header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,10 +7,10 @@ export const Header = () => {
   const gctx = useGlobalStateContext()
 
   return <Group justify="space-between" h="100%" px="md">
-    <Box>
+    <Group>
       <Burger opened={gctx.is_nav_open} hiddenFrom='sm' size="sm" onClick={gctx.on_nav_toggle}/>
       <Text size="xl" fw={700} c="primary.0">SoWee Event planner</Text>
-    </Box>
+    </Group>
     <Box pos="relative" top={-3} c="primary.0">
       { !gctx.is_mobile_screen && <SessionStatus text_color="primary.0"/> }
     </Box>
