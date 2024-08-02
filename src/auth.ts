c1f65eab833f3fd2fb54118ddd9f0ec3eb3cc47e
Aaron de Windt <aaron@delft-circuits.com>
2024-08-02 13:48:46 +0000

    Allow sosalsa.nl emails login access

--- src/auth.ts
+++ src/auth.ts
@@ -15,6 +15,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       const typed_user = user as Prisma.UserGetPayload<{}>
 
       if (typed_user.email === process.env.AUTH_SUPERUSER_EMAIL) return true
+      if (typed_user.email.toLowerCase().includes("@sosalsa.nl")) return true
 
       if (typed_user.is_superuser) return true
       if (!typed_user.is_verified) return "/unverified_user"
